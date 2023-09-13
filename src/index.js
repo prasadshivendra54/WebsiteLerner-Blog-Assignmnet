2c565c4018cbbd0fc65a79e0699f3787f65faa74
prasadshivendra54 <tshivendra07@gmail.com>
2023-09-13 13:50:33 +0530

    Add config folder

--- src/index.js
+++ src/index.js
@@ -2,18 +2,37 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes/route')
 
+const dotenv = require('dotenv') //
+const database = require('./config/database.js')
+
+const functions = require("firebase-functions") // added during deploy on firebse
+
 const app = express()
 
 app.use(express.json())
 
-// connect to databse
-mongoose.connect("mongodb://127.0.0.1:27017/websitelerner", {
-    useNewUrlParser: true
-}).then(() => {
-    console.log('Database connected')
-}).catch((error) => {
-    console.log(error.message)
-})
+// ------------------------ Dadabase -------------------------------------
+
+// connect to databse in Local - 
+// mongoose.connect("mongodb://127.0.0.1:27017/websitelerner", {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log('Database connected')
+// }).catch((error) => {
+//     console.log(error.message)
+// })
+
+
+// ------- OR ---------
+
+// config - 
+dotenv.config({ path: 'src/config/config.env' }) // set our config path
+
+// conect to database - 
+database()
+
+// ------------------------------------------------------------- 
+
 
 app.use('/', router)
 
@@ -22,3 +41,6 @@ const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`app is running on http://localhost:${PORT}`)
 })
+
+
+exports.api = functions.https.onRequest(app) // added during deploy on firebse
